802f916f5772f85f39500a757e60a5373c4a9507
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-17 15:05:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+/*function razzle() {
+    console.log("You've been razzled!");
+  }
+  razzle();*/
+  function saturdayFun(fun="roller-skate"){
+    return(`This Saturday, I want to ${fun}!`);
+
+  }
+  saturdayFun();
+  const  mondayWork=function(work="go to the office"){
+    return`This Monday, I will ${work}.`;
+  }
+  saturdayFun("from home");
+
+  function wrapAdjective(adjective="*"){
+    return function(name="special"){
+            return(`You are ${adjective}${name}${adjective}!`);
+          };
+    }
+console.log(wrapAdjective("%") ("a dedicated coder"));
+  
